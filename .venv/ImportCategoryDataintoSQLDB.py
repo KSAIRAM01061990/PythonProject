acba6064c6e83db7b59b71c0cf83807ea425e612
Sairam Kanamarlapudi <Sairam_Kanamarlapudi@epam.com>
2024-08-24 21:59:26 +0530

    Updated connectors.py

--- .venv/ImportCategoryDataintoSQLDB.py
+++ .venv/ImportCategoryDataintoSQLDB.py
@@ -1,24 +1,33 @@
 import pandas as pd
 import pyodbc
 import sqlalchemy as sa
+import pymysql
 
 # print(pyodbc.drivers())
+# print(sqlalchemy.drivers())
 
 servername='EPINHYDW015E'
 databasename='DEMO'
-tablename='CATEGORY3'
+tablename='CATEGORY'
 
 df1=pd.read_excel('C:\\Users\\Sairam_Kanamarlapudi\\Desktop\\Python\\CategoryData.xlsx')
+print(df1)
+df2=pd.read_csv('C:\\Users\\Sairam_Kanamarlapudi\\Desktop\\Python\\Data.csv')
+print(df2)
 engine=sa.create_engine(f'mssql+pyodbc://{servername}/{databasename}?driver=SQL+Server+Native+Client+11.0')
+df1.to_sql(tablename,engine,if_exists='append',index=False)
+print(engine)
 df1['fullname']=df1['firstname']+df1['lastname']
-# print(engine)
-# print(df1)
+print(engine)
+print(df1)
 pivot_data=df1.pivot_table(index='Category',columns='Year',values='Amount')
-# print(pivot_data)
+print(pivot_data)
 print(pivot_data.rename_axis(columns=None).reset_index())
 pivot_data.to_sql(tablename,engine,if_exists='append',index=False)
 
-# data = {
+
+
+# # data = {
 #          'name' : ['sai','ram','kumar'],
 #          'age'  : [20,30,40]
 #         }
