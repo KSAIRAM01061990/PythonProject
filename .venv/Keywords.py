be234e3bbd6defbbaf94bc1115c02f561fec7bde
Sairam Kanamarlapudi <Sairam_Kanamarlapudi@epam.com>
2024-09-12 21:55:14 +0530

    Keywords

---
+++ .venv/Keywords.py
@@ -0,0 +1,14 @@
+# a = int(input("enter insert value for adding : "))
+# b = int(input("insert value for adding : "))
+# c=a+b
+# print(c)
+
+print(abs(-65))
+
+# number methods
+import math
+a=17.8
+print(math.ceil(a))
+print(math.floor(a))
+print(math.sqrt(a))
+
