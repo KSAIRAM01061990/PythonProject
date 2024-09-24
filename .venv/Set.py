31e07a2e0a6028ae43c0013c312a72e8b06ba8e2
Sairam Kanamarlapudi <Sairam_Kanamarlapudi@epam.com>
2024-09-24 21:00:15 +0530

    Made some changes in set

--- .venv/Set.py
+++ .venv/Set.py
@@ -39,3 +39,44 @@ print(set1)
 #clear
 set1.clear()
 print(set1)
+
+# create a empty set
+a = set()
+print(type(a))
+
+set1 = {1,2,3,'a','b','c'}
+print(set1)
+# set don't allow the duplicate values
+set2 ={1,2,3,6,7,8,9,5,5,5}
+print(set2)
+
+# set methods
+
+set1={'a','b','c','d'}
+print(set1)
+
+# add()
+set1.add('k')
+print(set1)
+
+# pop()
+set1.pop()
+print(set1)
+
+# remove()
+set1.remove('k')
+#set1.remove('l')
+print(set1)
+
+# discord()
+set1.discard('l')
+print(set1)
+
+# clear(),Copy()
+# union
+set1={1,2,3,4,5,6,7}
+set2={'a','b','c','d'}
+print(set1.union(set2))
+set1.update(set2)
+print(set1)
+
